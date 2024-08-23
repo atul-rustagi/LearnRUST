7e4238d4ec2016a120022b37eb56a65f15ef3a7a
Atul Rustagi <06atul1998@gmail.com>
2024-08-23 12:37:48 +0530

    Removed todo target folder from git

--- todo/target/debug/deps/todo-93b5ae9e4ebc8d0e.d
+++
@@ -1,9 +0,0 @@
-C:\Users\atul.rustagi\Desktop\LearnRUST\todo\target\debug\deps\libtodo-93b5ae9e4ebc8d0e.rmeta: src\lib.rs src\things_todo.rs src\things_todo\items_completed.rs
-
-C:\Users\atul.rustagi\Desktop\LearnRUST\todo\target\debug\deps\libtodo-93b5ae9e4ebc8d0e.rlib: src\lib.rs src\things_todo.rs src\things_todo\items_completed.rs
-
-C:\Users\atul.rustagi\Desktop\LearnRUST\todo\target\debug\deps\todo-93b5ae9e4ebc8d0e.d: src\lib.rs src\things_todo.rs src\things_todo\items_completed.rs
-
-src\lib.rs:
-src\things_todo.rs:
-src\things_todo\items_completed.rs:
